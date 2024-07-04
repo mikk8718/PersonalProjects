c5dab15473e077ed6e22d9955e283143dd689703
mikk8718 <baunsgaard38@gmail.com>
2024-07-04 16:26:26 +0200

    Cleaned Twitch Fetcher, added error handling

--- TwitchFetcher/2_version.py
+++ TwitchFetcher/2_version.py
@@ -1,21 +1,14 @@
-from TwitchFetcher.TwitchNetworkElement import *
-from simple_term_menu import TerminalMenu
+from TwitchNetworkElement import *
 import subprocess 
-
-with TwitchNetworkElement("https://www.twitch.tv/directory/category/league-of-legends", 
-                        SearchType.LOW_TO_HIGH,
-                        ) as twe:
-        
-    url_base = "https://twitch.tv/"
-
-    streamer_menu = TerminalMenu(twe.streamers.format, title="Streams")
     
-    stream_menu_index = streamer_menu.show()
-
-    if(stream_menu_index == 0):
-        search = url_base+input("Streamer name: ")
-        
-    with subprocess.Popen(["streamlink", url_base + twe.streamers[stream_menu_index].handle, "best"],
+with (
+        TwitchNetworkElement("https://www.twitch.tv/directory/category/league-of-legends", 
+                             SearchType.LOW_TO_HIGH) as twe,
+        Menu(twe.streamers.format, "TwitchFetcher", twe) as menu
+    ):
+    
+    with subprocess.Popen(["streamlink", menu.target_streamer, "best"],
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
         stdout, stderr = p.communicate()
         
+    
--- TwitchFetcher/TwitchNetworkElement.py
+++ TwitchFetcher/TwitchNetworkElement.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from enum import Enum
+from simple_term_menu import TerminalMenu
+
 
 class NetworkElementError(Exception):
     pass
@@ -48,9 +50,6 @@ class TwitchNetworkElement(object):
     
     def __exit__(self, exctype, excinst, exectb):
         self.driver.quit()
-
-    def set_index(self, index):
-        self.index = index
         
     @property
     def streamers(self):
@@ -66,8 +65,43 @@ class TwitchNetworkElement(object):
 class Stream(object):
     def __init__(self, data):
         self.data = data
+        self.isFavorite = False
 
     @property
     def handle(self):
         return self.data
-   
+    
+class MenuError(Exception):
+    pass
+
+class Menu(object):
+    def __init__(self, options, title, twe):
+        try:
+            self.twe = twe
+            self.menu = TerminalMenu(options, title=title)
+            self.index = self.menu.show()
+            self.validate_index(self.index)
+        except:
+            raise MenuError("Couldn't load menu")    
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exctype, excinst, exectb):
+        pass
+    
+    @property
+    def target_streamer(self):
+        if not hasattr(self, 'handle'):
+            raise MenuError("Error")
+        return "https://www.twitch.tv" + getattr(self, 'handle')
+    
+    def validate_index(self, index):
+        if not isinstance(self.index, int):
+            raise MenuError("Index not loaded properly")
+        if self.index < 0:
+            raise MenuError("Can't use index")
+        if self.index == 0:
+            setattr(self, 'handle', "/" + input("Search streamer: "))
+        else:
+            setattr(self, 'handle', self.twe.streamers[self.index].handle)
